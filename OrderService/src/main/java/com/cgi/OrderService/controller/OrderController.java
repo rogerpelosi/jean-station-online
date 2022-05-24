b61b1f67d92fd65a8249caa36e820f1a4edff74c
Pelosi <roger.pelosi@cgi.com>
2022-05-24 14:54:31 -0400

    basic functionality of order service is working in postman

--- OrderService/src/main/java/com/cgi/OrderService/controller/OrderController.java
+++ OrderService/src/main/java/com/cgi/OrderService/controller/OrderController.java
@@ -5,15 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cgi.OrderService.exceptions.OrderWithIDExists;
 import com.cgi.OrderService.exceptions.OrderWithIDNotFound;
 import com.cgi.OrderService.models.Order;
-import com.cgi.OrderService.repository.OrderRepository;
 import com.cgi.OrderService.service.OrderService;
 
 @RestController
@@ -32,7 +35,7 @@ public class OrderController {
 	}
 	
 	@GetMapping("/orders/{orderId}")
-	public ResponseEntity<?> getOrderById(@PathVariable("orderId") int id){
+	public ResponseEntity<?> getOrderByIdHandler(@PathVariable("orderId") int id){
 		ResponseEntity<?> responseEntity;
 		try {
 			Order order = orderService.getOrderByID(id);
@@ -42,5 +45,44 @@ public class OrderController {
 		}
 		return responseEntity;
 	}
+	
+	@PostMapping("/orders")
+	public ResponseEntity<?> addNewOrderHandler(@RequestBody Order newOrder){
+		ResponseEntity<?> responseEntity;
+		try {
+			Order order = orderService.addNewOrder(newOrder);
+			responseEntity = new ResponseEntity<Order>(order, HttpStatus.CREATED);
+		} catch(OrderWithIDExists e) {
+			responseEntity = new ResponseEntity<String>("Duplicate Resource", HttpStatus.CONFLICT);
+		}
+		return responseEntity;
+	}
+	
+	@PutMapping("/orders/{orderId}")
+	public ResponseEntity<?> updateOrderHandler(@PathVariable("orderId") int id, @RequestBody Order updatedOrder){
+		ResponseEntity<?> responseEntity;
+		try {
+			Order order = orderService.editOrderByID(updatedOrder);
+			responseEntity = new ResponseEntity<Order>(order, HttpStatus.OK);
+		} catch(OrderWithIDNotFound e) {
+			responseEntity = new ResponseEntity<String>("Order Not Found", HttpStatus.NOT_FOUND);
+		}
+		return responseEntity;
+	}
+	
+	@DeleteMapping("/orders/{orderId}")
+	public ResponseEntity<String> deleteOrderHandler(@PathVariable("orderId") int id){
+		ResponseEntity<String> responseEntity;
+		try {
+			orderService.deleteOrderByID(id);
+			responseEntity = new ResponseEntity<String>("Order Deleted", HttpStatus.OK);
+		} catch(OrderWithIDNotFound e) {
+			responseEntity = new ResponseEntity<String>("Order Not Found", HttpStatus.NOT_FOUND);
+		}
+		return responseEntity;
+	}
+	
+	@GetMapping("/orders/status")
+	public ResponseEntity<List<Order>>
 
 }
